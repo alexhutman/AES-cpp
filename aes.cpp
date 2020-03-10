deadcec0532c3efbe5d371288299cc30081e312c
Alex <alexhutman@gmail.com>
2020-03-09 21:13:43 -0400

    figuring out how uint bit shifting works lol

--- aes.cpp
+++ aes.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <cstdio>
+#include <cinttypes>
 using namespace std;
 
 int main(int argc, char* argv[]) {
 	cout << "Hello world!" << endl;
+
+	uint8_t jeff = 0xFF;
+
+	cout << "Jeff: " << unsigned(jeff) << endl;
+
+	jeff <<= 5;
+
+	cout << "Jeff 2: " << unsigned(jeff) << endl;
 
 	return 0;
 }
